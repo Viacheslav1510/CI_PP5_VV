2fd514749f2594976d7302bfcb2507cb62279bd7
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-04-20 13:45:49 +0100

    customize admin products

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Album, Genre
 
 
-admin.site.register(Album)
-admin.site.register(Genre)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = (
+        'genre',
+        'name',
+        'sku',
+        'price',
+        'rating',
+    )
+
+    ordering = ('genre', 'name')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Genre, GenreAdmin)
