70b533a0a3ad4973717bc818d76db8a772497b02
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-04-20 17:52:31 +0100

    add product details page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 
@@ -8,3 +8,11 @@ def all_products(request):
         'albums': albums,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    album = get_object_or_404(Album, pk=product_id)
+    context = {
+        'album': album,
+    }
+    return render(request, 'products/product_detail.html', context)
