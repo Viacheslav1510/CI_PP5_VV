d4c3278f762dc8430820964e92f269759a68cbdf
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-04-20 13:37:48 +0100

    add products app, create album and genre models, register models in admin

---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Album(models.Model):
+    genre = models.ForeignKey(
+        'Genre',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL)
+    sku = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True
+        )
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+        )
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=1,
+        null=True,
+        blank=True
+        )
+    image = models.ImageField(
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.name
