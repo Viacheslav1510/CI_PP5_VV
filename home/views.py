8ea8667e01bf58ce4bbcb78ca699c83f64dccda7
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-04-21 13:04:32 +0100

    add genres filtering

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from products.models import Album, Genre
 
 
 def index(request):
-    
-    return render(request, 'home/index.html')
+    albums = Album.objects.all()
+    genres_list = Genre.objects.all()
+
+    context = {
+        'albums': albums,
+        'genres_list': genres_list,
+    }
+    return render(request, 'home/index.html', context)
--- products/views.py
+++ products/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Album
+from .models import Album, Genre
 
 
 def all_products(request):
     albums = Album.objects.all()
+    genres_list = Genre.objects.all()
     query = None
+    genre = None
 
     if request.GET:
+        if 'genre' in request.GET:
+            genres = request.GET['genre'].split(',')
+            albums = albums.filter(genre__name__in=genres)
+            genres = Genre.objects.filter(name__in=genres)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,7 +30,8 @@ def all_products(request):
 
     context = {
         'albums': albums,
-        'search_term': query
+        'search_term': query,
+        'genres_list': genres_list,
     }
     return render(request, 'products/products.html', context)
 
