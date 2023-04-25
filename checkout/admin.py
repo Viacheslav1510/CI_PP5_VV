ab2fe4d89a6626aa34d83bec93fe9f0c123da41c
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-04-25 12:45:37 +0100

    add admin models, add signals to update order grand total on each item added

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,54 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd Party
 from django.contrib import admin
+from django.contrib import admin
+# Internal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import Order, OrderLineItem
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+    fields = (
+        'order_number',
+        'date',
+        'full_name',
+        'email',
+        'phone_number',
+        'address1',
+        'address2',
+        'town_or_city',
+        'postcode',
+        'county',
+        'country',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
