7651caedae894e872ed1c14168e82c95a5ee024c
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-04-26 12:16:56 +0100

    add stripe card element

--- checkout/views.py
+++ checkout/views.py
@@ -13,5 +13,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MwN0BBBWQhjq9UyATE3hTvpSODH77g6PQ67n5wxc7DkSdfXFsj8AHmaFJjaQ9yyRIhiEENhRSiybhh9tBS1AKLd00R9wtw6Sq',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
