5b78c1f5ffb27ce864070e1ea0c01d5169e3ddf0
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-04-28 13:38:20 +0000

    fix bug with webhooks

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -27,12 +27,16 @@ class StripeWH_Handler:
         """
         intent = event.data.object
         pid = intent.id
-        bag = intent.metadata.basket
+        bag = intent.metadata.bag
         save_info = intent.metadata.save_info
+        
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
 
-        billing_details = intent.charges.data[0].billing_details
+        billing_details = stripe_charge.billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2)
 
         for field, value in shipping_details.address.items():
             if value == "":
