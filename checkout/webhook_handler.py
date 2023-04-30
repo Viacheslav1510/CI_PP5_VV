52ca6f21021c3d92f96a0594c72b638dcd6956a4
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-04-30 21:13:43 +0100

    update webhook hadler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,6 +5,7 @@ import stripe
 
 from .models import Order, OrderLineItem
 from products.models import Album
+from profiles.models import UserProfile
 
 
 class StripeWH_Handler:
@@ -41,6 +42,20 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_address1 = shipping_details.address.line1
+                profile.default_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -102,7 +117,8 @@ class StripeWH_Handler:
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=f'Webhook received: {event["type"]} | '
+            'SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
