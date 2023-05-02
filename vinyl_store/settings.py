575253ceacae80978ffbe4ea195e032722cd747a
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-05-02 10:00:49 +0000

    test deployment

--- vinyl_store/settings.py
+++ vinyl_store/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.exists("env.py"):
     import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-n78eo&w6ykv*q0o4qf*vrl5t*n5v&2!-@jm1esxf_1#pv5s1xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ci-tvv-pp5.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,13 +119,21 @@ WSGI_APPLICATION = 'vinyl_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
+# DATABASES = {
+#      'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#  }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
