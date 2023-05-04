7d11859818364955e5b850bb9bd5cc585eadfc25
Viacheslav1510 <79586875+Viacheslav1510@users.noreply.github.com>
2023-05-04 13:09:17 +0100

    add contact form, create templates, add view for contact from and message received

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+from .forms import ContactForm
+
 
 def get_user_instance(request):
     """
@@ -14,8 +16,21 @@ def get_user_instance(request):
 
 
 def contact_view(request):
-
+    if request.method == 'POST':
+        email = request.user.email
+        contact_form = ContactForm(request.POST, initial={'email': email})
+        if contact_form.is_valid():
+            contact = contact_form.save(commit=False)
+            contact.user = request.user
+            contact.save()
+            messages.info(request, "Massage has been sent")
+        return render(request, 'contact/message-received.html')
+    else:
+        email = request.user.email
+        contact_form = ContactForm(initial={'email': email})
     template = 'contact/contact.html'
-    context = {}
+    context = {
+        'contact_form': contact_form
+    }
 
     return render(request, template, context)
--- vinyl_store/urls.py
+++ vinyl_store/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
     path('blog/', include('blog.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
